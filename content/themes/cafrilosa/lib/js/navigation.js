ef2b6c5b5fc18bb6480e23ad543cdea0848d5d04
Miguel Alejandro <mvinan@icloud.com>
2015-12-30 12:21:12 -0500

    fixed minor bugs navigation

--- content/themes/cafrilosa/lib/js/navigation.js
+++ content/themes/cafrilosa/lib/js/navigation.js
@@ -2,11 +2,22 @@
   'use strict';
 
   var btnMenu = '.btn-menu',
-      hamburguerClasses = 'glyphicon-menu-hamburger glyphicon-remove';
+      hamburguerClasses = 'icon-icon-menu glyphicon-remove';
 
   $(document).on('click', btnMenu, function (e) {
     $(e.originalEvent.target).toggleClass(hamburguerClasses);
     $('.nav .nav-list').toggleClass('is-active');
     return false;
   });
+
+  $(window).on("scroll", stikyMenu );
+
+	function stikyMenu(){
+		if ( $(window).scrollTop() > 1 ){
+			$(".navigation").addClass("menu-fixed");
+		}
+		else{
+			$(".navigation").removeClass("menu-fixed");
+		}
+	}
 })();
