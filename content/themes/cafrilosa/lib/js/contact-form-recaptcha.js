1b308514e06fcfbc5bd4cd35b04bddb0e00b27ba
Mario Andrés Correa <a@macool.me>
2016-01-21 20:39:30 -0500

    refactor contact form & use recaptcha

--- content/themes/cafrilosa/lib/js/contact-form-recaptcha.js
+++ content/themes/cafrilosa/lib/js/contact-form-recaptcha.js
@@ -1,14 +1,38 @@
 (function () {
   'use strict';
 
-  // to toggle recaptcha
+  var ContactFormRecaptcha = function () {
 
-  var btnSelector = '.submit-contact-form-btn',
-      recaptchaWrapper = '.cafrilosa-recaptcha-wrapper';
+    this.recaptchaWrapper = '.cafrilosa-recaptcha-wrapper';
+    this.eventListeners();
+  };
 
-  $(document).on('click', btnSelector, function () {
-    $(recaptchaWrapper).removeClass('hidden');
-    $(this).remove();
+  ContactFormRecaptcha.prototype.eventListeners = function () {
+    $(document).on(
+      'contactForm:showRecaptcha',
+      $.proxy(this.showRecaptcha, this)
+    );
+    $(document).on(
+      'contactForm:clearInputs',
+      $.proxy(this.clearInputs, this)
+    );
+  };
+
+  ContactFormRecaptcha.prototype.clearInputs = function () {
+    grecaptcha.reset();
+    $(this.recaptchaWrapper).remove();
+  };
+
+  ContactFormRecaptcha.prototype.showRecaptcha = function () {
+    $(this.recaptchaWrapper).removeClass('hidden')
+                            .find("input[type='submit']").hide();
     return false;
-  });
+  };
+
+  ContactFormRecaptcha.prototype.recaptchaOkay = function () {
+    $(document).trigger('contactForm:submit');
+  };
+
+  var contactFormRecaptcha = new ContactFormRecaptcha();
+  window.recaptchaOkay = contactFormRecaptcha.recaptchaOkay;
 })();
